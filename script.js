305bb278284f229c2f2977aba0e809db4dcf8767
Nite <solitude.loner7@gmail.com>
2021-01-10 23:36:12 +0700

    [Refactor + Fix] Clear un-used HTML + added hover effect

---
+++ script.js
@@ -0,0 +1,14 @@
+const app = (() => {
+    const _form = document.querySelector(".container__content__main__form");
+    const _emailInput = document.querySelector(".container__content__main__form__email");
+
+    _form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let email = _emailInput.value;
+
+        
+    })
+
+    
+})()
