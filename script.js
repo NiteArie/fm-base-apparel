b51d407acf0c4d713492ee0e804d54ff1d578e57
Nite <solitude.loner7@gmail.com>
2021-01-11 00:22:40 +0700

    [Fea] Form email validation

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const app = (() => {
     const _form = document.querySelector(".container__content__main__form");
     const _emailInput = document.querySelector(".container__content__main__form__email");
+    const _emailError = document.querySelector(".container__content__main__error");
+    const _regex = /^\S+@\S+$/;
 
     _form.addEventListener("submit", (event) => {
         event.preventDefault();
 
         let email = _emailInput.value;
 
+        if (validEmail(email)) {
+            hideEmailError();
+        } else {
+            showEmailError();
+        }
         
     })
 
-    
+    function validEmail(email) {
+        return _regex.test(email);
+    }
+
+    function showEmailError() {
+        _emailError.classList.remove("hidden");
+        _emailInput.classList.add("error");
+    }
+
+    function hideEmailError() {
+        _emailError.classList.add("hidden");
+        _emailInput.classList.remove("error");
+    }
+
+
 })()
